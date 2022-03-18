8375ce5515a6a97db4f8b84743acbec6ab25bd2d
PavM9 <p.i.myasnikov@mail.ru>
2022-03-18 22:49:39 +0300

    feat: добавить popup новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,61 @@
 const editProfileButton = document.querySelector('.profile__edit-button')
-const editProfilePopup = document.querySelector('.popup')
-const popupCloseButton = editProfilePopup.querySelector('.popup__close-button')
-const popupForm = editProfilePopup.querySelector('.popup__form-container')
+const addCardButton = document.querySelector('.profile__add-card-button')
+const popupElement = document.querySelectorAll('.popup')
+const editProfilePopup = document.querySelector('.popup_type_edit-profile')
+const addCardPopup = document.querySelector('.popup_type_add-card')
+const popupCloseButton = document.querySelector('.popup__close-button')
+const popupForm = document.querySelector('.popup__form-container')
 let popupProfileName =  popupForm.querySelector('.popup__form-item_input_name')
 let popupProfileDescription = popupForm.querySelector('.popup__form-item_input_description')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 
-const openPopup = function() {
-  editProfilePopup.classList.add('popup_is-opened')
-}
-const closePopup = function() {
-  editProfilePopup.classList.remove('popup_is-opened')
-}
-const closePopupByOverlay = function(event) {
-   if (event.target !== event.currentTarget) {
-    return
+const initialCards = [
+  {
+    name: 'Териберка',
+    link: 'images/teriberka.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'images/kamchatka.jpg'
+  },
+  {
+    name: 'Кабардино-Балкария',
+    link: 'images/kabardino-balkaria.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: 'images/altai.jpg'
+  },
+  {
+    name: 'Урал',
+    link: 'images/ural.jpg'
+  },
+  {
+    name: 'Карелия',
+    link: 'images/karelia.jpg'
   }
-  closePopup()
+];
+
+const openPopup = function(popup) {
+  popup.classList.add('popup_is-opened')
 }
+const closePopup = function(popup) {
+  popup.classList.remove('popup_is-opened')
+}
+
+const closePopupByClick = function(popup) {
+  popup.addEventListener('click', function (event) {
+    if (event.target.classList.contains('popup_is-opened')) {
+      closePopup(popup);
+    };
+    if (event.target.classList.contains('popup__close-button')) {
+      closePopup(popup);
+    };
+  });
+};
+popupElement.forEach(closePopupByClick);
+
 const addInputValues = function() {
   popupProfileName.value = profileName.textContent
   popupProfileDescription.value = profileDescription.textContent
@@ -33,10 +70,13 @@ function formSubmitHandler (event) {
 
 popupForm.addEventListener('submit', formSubmitHandler)
 editProfileButton.addEventListener('click', function() {
-  openPopup()
+  openPopup(editProfilePopup)
   addInputValues()
-})
-popupCloseButton.addEventListener('click', closePopup)
-editProfilePopup.addEventListener('click', closePopupByOverlay)
+});
+addCardButton.addEventListener('click', function() {
+  openPopup(addCardPopup)
+});
+
+
 
 
